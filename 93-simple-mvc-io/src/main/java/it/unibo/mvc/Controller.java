32e0182c34461a3fbf305898b19484a8b7c2233b
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-11-23 22:17:54 +0100

    Ex 93, printed on file

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,43 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private Optional<File> currentFile;
 
+    public void setFile(File file) {
+        currentFile = Optional.of(file);
+    }
+
+    public File getFile() {
+        return currentFile.get();
+    }
+
+    public String getPath() {
+        return currentFile.get().getAbsolutePath();
+    }
+
+    public void printOnFile(final String input) {
+        try {
+            Files.write(currentFile.get().toPath(), Objects.requireNonNull(input).getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            
+        }
+    }
 }
