cf2fceee6d91bbce562fa6aac8440ece74fec9d6
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-11-25 17:16:27 +0100

    Ex 92 complete

--- 92-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 92-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -12,14 +12,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.List;
 import java.util.Random;
 
 /**
