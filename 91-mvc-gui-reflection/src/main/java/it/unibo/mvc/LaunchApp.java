8a3bf33fb9a7d238f07478ff682bdb61d8967ecc
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-11-25 18:15:19 +0100

    Ex 91 compoleted

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,6 +1,10 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberOutputView;
@@ -11,24 +15,34 @@ import it.unibo.mvc.view.DrawNumberSwingView;
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    private LaunchApp() {
+    }
 
     /**
      * Runs the application.
      *
      * @param args ignored
-     * @throws ClassNotFoundException if the fetches class does not exist
-     * @throws NoSuchMethodException if the 0-ary constructor do not exist
+     * @throws ClassNotFoundException    if the fetches class does not exist
+     * @throws NoSuchMethodException     if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
-     * @throws InstantiationException if the constructor throws exceptions
-     * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
+     * @throws InstantiationException    if the constructor throws exceptions
+     * @throws IllegalAccessException    in case of reflection issues
+     * @throws IllegalArgumentException  in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws ClassNotFoundException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberOutputView());
-        
+        final String PACKAGE = "it.unibo.mvc.view.";
+        final List<String> classList = List.of(PACKAGE + "DrawnumberSwingView", PACKAGE + "DrawnumberOutputView");
+        for (final String className : classList) {
+            final Class<?> member = Class.forName(className);
+            final Object view = member.getConstructor().newInstance();
+            app.addView((DrawNumberView) view);
+        }
+
     }
 }
